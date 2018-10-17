06ee0cae238de42b6b5068a5f810714e454f89ae
Atishay Jain <contact@atishay.me>
2018-10-17 06:52:19 -0700

    Move tests to the test folder.

--- test/index.js
+++ test/index.js
@@ -5,13 +5,13 @@ const fs = require("fs");
  */
 
 // 1. Module that returns the version number of ImageMagick.
-const versionFinder = require("./build/Release/Version.node");
+const versionFinder = require("../build/Release/Version.node");
 console.info("ImageMagick Version - ", versionFinder.version());
 
 // 2. Pass an image to get info.
 // Don't use Sync on production. This is for demo purposes.
-const image = fs.readFileSync('logo.png');
-const infoProvider = require("./build/Release/Info.node");
+const image = fs.readFileSync(__dirname + '/photo.jpg');
+const infoProvider = require("../build/Release/Info.node");
 const processed = infoProvider.processImage(image);
 // JavaScript has a JSON beautifier built in. The third argument
 // is the number of characters to use as whitespace -
@@ -23,8 +23,8 @@ console.info(JSON.stringify(processed, 0, 2));
 // Using callbacks as we wait on https://github.com/nodejs/node-addon-api/issues/231
 // N API is hot of the machine.
 // Once that is resolved it will be async await here.
-const photo = fs.readFileSync('photo.jpg');
-const editor = require("./build/Release/Edit.node");
+const photo = fs.readFileSync(__dirname + '/photo.jpg');
+const editor = require("../build/Release/Edit.node");
 editor.edit(photo, (err, processed) => {
-  fs.writeFileSync('edit.jpg', processed);
+  fs.writeFileSync(__dirname + '/edit.jpg', processed);
 });
