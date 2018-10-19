c8c3d1e74c288bb6210290a518ad0d6a3f6c7dd0
Atishay Jain <contact@atishay.me>
2018-10-18 20:08:08 -0700

    Added working code on the server.

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const editor = require('./build/Release/Edit.node')
+const editor = require('./build/Release/Edit.node');
+var bodyParser = require('body-parser');
 const app = express();
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/', (req, res) => {
-  editor.edit(Buffer.from(req.body, 'base64'), (buffer) => {
+app.post('/', bodyParser.text({ limit: '50mb' }), (req, res) => {
+  const requestBuffer = Buffer.from(req.body.split(",")[1], 'base64');
+  editor.edit(requestBuffer, (err, buffer) => {
     res.send("data:image/jpeg;base64," + buffer.toString('base64'));
   })
 });
